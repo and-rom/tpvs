63ae89743e6f105eb28f0b79cbbef6a5c6f58af5
Andrey Romanov <andrey.a.romanov@gmail.com>
2019-06-04 20:08:02 +0300

    Change URL for downloading tumblr package

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TMPFILE=`mktemp`
 PWD=`pwd`
-wget -qO $TMPFILE "https://php-download.com"$(wget -qO - "https://php-download.com/downloads/?version=0.4.0.0&package=tumblr&vendor=tumblr&download_type=REQUIRE" | sed -ne "s/^.*window.location.href = '\(.*\)';.*$/\1/p")
+wget -qO $TMPFILE "https://php-download.com"$(wget -qO - "https://php-download.com/downloads/download?vendor=tumblr&package=tumblr&version=0.4.0.0&downloadType=REQUIRE" | sed -ne "s/^.*window.location.href = '\(.*\)';.*$/\1/p")
 unzip -qq -d $PWD $TMPFILE "*.php" -x "index.php"
 rm $TMPFILE
 
