e94f94e300283f0584e95f2112dd1f7a19ee77b4
Andrey Romanov <andrey.a.romanov@gmail.com>
2020-09-23 14:05:59 +0300

    Change download URL fog js.cookie.js

--- install.sh
+++ install.sh
@@ -8,4 +8,4 @@ rm $TMPFILE
 mkdir -p ./js
 wget -qP ./js/ "https://code.jquery.com/jquery-3.3.1.min.js"
 
-wget -qP ./js/ "https://raw.githubusercontent.com/js-cookie/js-cookie/master/src/js.cookie.js"
+wget -qO ./js/js.cookie.js "https://github.com/js-cookie/js-cookie/releases/download/v2.2.0/js.cookie-2.2.0.min.js"
