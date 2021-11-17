1bb6e2e483ad93cc175b11aa8a8d787e8e87c682
Andrey Romanov <andrey.a.romanov@gmail.com>
2021-11-17 22:34:25 +0300

    Switch to composer

--- install.sh
+++ install.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-TMPFILE=`mktemp`
-PWD=`pwd`
-wget -qO $TMPFILE "https://php-download.com"$(wget -qO - "https://php-download.com/downloads/download?vendor=tumblr&package=tumblr&version=0.4.0.0&downloadType=REQUIRE" | sed -ne "s/^.*window.location.href = '\(.*\)';.*$/\1/p")
-unzip -qq -d $PWD $TMPFILE "*.php" -x "index.php"
-rm $TMPFILE
-
 mkdir -p ./js
 wget -qP ./js/ "https://code.jquery.com/jquery-3.3.1.min.js"
 
